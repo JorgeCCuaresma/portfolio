1fc0f36a233188153cc395cff2c247794cc02a77
Jorge Cuesta <jorgecuesta.90@gmail.com>
2023-02-03 12:42:00 +0100

    add styles at navBar

--- portfolio/src/Components/navBar/navBar.js
+++ portfolio/src/Components/navBar/navBar.js
@@ -1,27 +1,28 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import styles from './navBar.module.css'
 
 
 const NavigationBar = () => {
     return (
-        <Navbar collapseOnSelect expand="lg" bg="light" >
-            <Container>
-                <Navbar.Brand href="#home">Cuaresma Developer</Navbar.Brand>
+        <Navbar className={styles.navBar}  collapseOnSelect expand="lg" bg="light" >
+            <Container  >
+                <Navbar.Brand  href="#home"><span className={styles.font}>Cuaresma Developer</span></Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="me-auto">
                     </Nav>
                     <Nav>
-                        <Nav.Link href="#deets">About</Nav.Link>
+                        <Nav.Link href="#deets"><span className={styles.font}>About</span></Nav.Link>
                         <Nav.Link href="#memes">
-                            Skills
+                        <span className={styles.font}>Skills</span>
                         </Nav.Link>
                         <Nav.Link href="#memes">
-                            Projects
+                        <span className={styles.font}>Projects</span>
                         </Nav.Link>
-                        <Nav.Link href="#memes">
-                            Contact
+                        <Nav.Link  href="#memes">
+                        <span className={styles.font}>Contact</span>
                         </Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
