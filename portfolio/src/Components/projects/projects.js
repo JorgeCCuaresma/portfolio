0bcfe1b89c939736b58a0c90512d72b0a6cc9552
Jorge Cuesta <jorgecuesta.90@gmail.com>
2023-03-13 14:53:26 +0100

    creat Projects component

---
+++ portfolio/src/Components/projects/projects.js
@@ -0,0 +1,13 @@
+import styles from './projects.module.css'
+
+const Projects = () =>{
+
+    return(
+        <div className={styles.container}>
+
+        </div>
+    )
+}
+
+
+export default Projects
