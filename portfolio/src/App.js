b23e8d57b85c677d8dc18af74f3ccf341ee5491c
Jorge Cuesta <jorgecuesta.90@gmail.com>
2023-03-13 14:52:34 +0100

    react-router-dom

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,16 +1,21 @@
 import './App.css';
 import NavigationBar from './Components/navBar/navBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import About from './Components/about/about';
+import Main from './Components/main/main.js';
+import { Routes, Route } from "react-router-dom";
+import Projects from './Components/projects/projects';
 
 
 
 
 function App() {
   return (
-    <>    
-      <NavigationBar /> 
-      <About/>   
+    <>
+      <NavigationBar />
+      <Routes>
+        <Route path='/' element={<Main/>}/>
+        <Route path='/projects' element={<Projects/>}/>
+      </Routes>
     </>
   );
 }
